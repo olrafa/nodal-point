327e63ee2037718524ef716e3589d3abb2e602d6
Rafael Olivares <rafaelrolivares@gmail.com>
2023-04-27 19:28:55 -0300

    Clears consoles and removes Python file

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,17 @@ const playRandomGame = () => {
   const [firstPlayer, secondPlayer] = getRandomPlayers();
 
   const match = createMatch(firstPlayer, secondPlayer);
-  console.log(match);
+
+  const { p1, p2 } = match;
+
+  const [playerOne, playerTwo] = [p1, p2].map(
+    ({ firstName, lastName, ranking }) =>
+      `${firstName} ${lastName} (${ranking})`
+  );
+
+  console.log(`\nToday's match is between ${playerOne} and ${playerTwo} \n`);
 
   playMatch(match);
-
 };
 
 playRandomGame();
--- src/printScoreboard.ts
+++ src/printScoreboard.ts
@@ -2,11 +2,35 @@ import { POINT_SYSTEM } from "./constants";
 import { Match, Player, PlayerScore } from "./types";
 
 export const printScoreBoard = (match: Match, isTieBreak = false) => {
-  const score = [
+  const scores = [
     createScoreLine(match.p1, isPlayerServing(match.p1, match), isTieBreak),
     createScoreLine(match.p2, isPlayerServing(match.p2, match), isTieBreak),
   ];
-  console.table(score);
+
+  const trimmedScore = scores.map((score) => {
+    if (match.set === 1) {
+      const { S1, S2, S3, ...info } = score;
+      return info;
+    }
+    if (match.set === 2) {
+      const { S2, S3, ...info } = score;
+      return info;
+    }
+    if (match.set === 3) {
+      const { S3, ...info } = score;
+      return info;
+    }
+    if (!match.ongoing) {
+      const { games, points, service, ...info } = score;
+      if (!match.p1.gamesS3 && !match.p2.gamesS3) {
+        const { S3, ...finalScore } = info;
+        return finalScore;
+      }
+      return info;
+    }
+  });
+
+  console.table(trimmedScore);
 };
 
 const isPlayerServing = (player: Player, match: Match) =>
@@ -17,10 +41,10 @@ const createScoreLine = (
   serving: boolean,
   isTieBreak: boolean
 ) => ({
-  player: `${player.lastName} (${player.ranking})`,
-  s1: player.gamesS1,
-  s2: player.gamesS2,
-  s3: player.gamesS3,
+  name: `${player.lastName} (${player.ranking})`,
+  S1: player.gamesS1,
+  S2: player.gamesS2,
+  S3: player.gamesS3,
   games: player.games,
   points: isTieBreak ? player.points : POINT_SYSTEM[player.points],
   ...(serving && { service: "*" }),
