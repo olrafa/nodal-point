eb00957dc9fd906c24b2aa961acd95cc3920dd43
Rafael Olivares <rafaelrolivares@gmail.com>
2023-06-29 13:46:50 -0300

    Fix WTA names

--- src/scraper/constants.ts
+++ src/scraper/constants.ts
@@ -3,23 +3,7 @@ export const ORGS = ["WTA", "ATP"] as const;
 // So we never run the risk of mis-spelling WTA or ATP;
 export type Org = (typeof ORGS)[number];
 
-type PageItem = {
-  landingPage: string;
-  targetClass: string;
-  attribute: string;
-};
-
-export const ORG_PATHS: { [key in Org]: PageItem } = {
-  WTA: {
-    landingPage: "https://www.wtatennis.com/rankings/singles",
-    targetClass: "a.rankings__player-link",
-    attribute: "title",
-  },
-  ATP: {
-    landingPage: "https://www.atptour.com/en/rankings/singles",
-    targetClass: "span.player-cell-wrapper > a",
-    attribute: "ga-label",
-  },
-};
+export const WTA_LANDING_PAGE = "https://www.wtatennis.com/rankings/singles";
+export const ATP_LANDING_PAGE = "https://www.atptour.com/en/rankings/singles";
 
 export const MAX_PLAYERS = 16; // Otherwise ATP will return 100 players.
--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -2,37 +2,80 @@ import puppeteer from "puppeteer";
 
 import { Player } from "../types";
 
-import { MAX_PLAYERS, Org, ORG_PATHS } from "./constants";
+import {
+  ATP_LANDING_PAGE,
+  MAX_PLAYERS,
+  Org,
+  WTA_LANDING_PAGE,
+} from "./constants";
 
 /**
  * Here we will retrieve the up-to-date rankings from WTA and ATP.
  */
 
 const getRankedPlayers = async (org: Org) => {
-  const info = ORG_PATHS[org];
-  const { landingPage, targetClass, attribute } = info;
+  if (org === "WTA") {
+    return await getWtaPlayers();
+  }
+  return await getAtpPlayers();
+};
+
+/**
+ * Retrieve names and rankings from the WTA website.
+ * @returns
+ */
+const getWtaPlayers = async () => {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  await page.goto(landingPage);
+  await page.goto(WTA_LANDING_PAGE);
 
-  const playerRows = await page.$$eval(
-    targetClass,
-    (rows, prop) => rows.map((row) => row.getAttribute(prop)),
-    attribute
-  );
+  const getCellContent = async (cssClass: string) =>
+    (await page.$$eval(cssClass, (rows) =>
+      rows.map((row) => row.innerHTML)
+    )) as string[];
 
-  // Currently the WTA page will return duplicate values,
-  // still in search of a better class.
-  const playerNames = org === "WTA" ? [...new Set(playerRows)] : playerRows;
+  const firstNames = await getCellContent(".player-name__first-name");
+  const lastNames = await getCellContent(".player-name__last-name");
+
+  const playerNames: string[][] = firstNames.map((_, i) => [
+    firstNames[i],
+    lastNames[i],
+  ]);
 
   await page.close();
   await browser.close();
 
-  return createPlayersArray(playerNames);
+  return playerNames
+    .map(([firstName, lastName], i) => ({
+      fullName: `${firstName} ${lastName}`,
+      lastName,
+      ranking: i + 1,
+    }))
+    .slice(0, MAX_PLAYERS);
 };
 
-// Not perfect since this will not take into account names like
-// Beatriz Haddad Maia or Juan Martín del Potro.
+/**
+ * Retrieves names and ranking from the ATP website.
+ * @returns
+ */
+const getAtpPlayers = async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto(ATP_LANDING_PAGE);
+
+  const playerRows = await page.$$eval(
+    "span.player-cell-wrapper > a",
+    (rows, prop) => rows.map((row) => row.getAttribute(prop)),
+    "ga-label"
+  );
+
+  await page.close();
+  await browser.close();
+
+  return createPlayersArray(playerRows);
+};
+
+// Still need a better way to construct ATP last names
 const createPlayersArray = (names: string[]): Player[] =>
   names
     .map((name, i) => {
